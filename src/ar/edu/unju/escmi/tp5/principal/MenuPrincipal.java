850ef5cfce522b457a4b8a4125db07e30f09d1b0
lujancansino <celestelujancansino@gmail.com>
2025-10-04 10:42:03 -0300

    DiagramaUML(correcciones)- Cansino Lujan y sofia rodriguez

---
+++ src/ar/edu/unju/escmi/tp5/principal/MenuPrincipal.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.escmi.tp5.principal;
+import java.util.Scanner;
+
+public class MenuPrincipal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int op;
+        do{
+            System.out.println("Sistema de Ventas");
+            System.out.println("1- Cliente");
+            System.out.println("2- Encargado de ventas");
+            System.out.println("3- Agente administrativo");
+            System.out.println("4- Salir");
+            System.out.println("Elige una opcion:");
+            op = sc.nextInt(); sc.nextLine();     
+
+            switch(op){
+                case 1: SubmenuCliente.main(args);; break;
+                case 2: SubmenuEncargVentas.main(args); break;
+                case 3: SubmenuAgenteAdmin.main(args);; break;
+                case 4: System.out.println("Saliendo del sistema"); break;
+                default: System.out.println("Opcion invalida"); break;
+            }
+        }while(op!=4);
+
+        sc.close();
+    }
+}
---
+++ src/ar/edu/unju/escmi/tp5/principal/SubmenuCliente.java
@@ -0,0 +1,45 @@
+package ar.edu.unju.escmi.tp5.principal;
+
+import java.util.Scanner;
+
+import ar.edu.unju.escmi.tp5.collections.CollectionCliente;
+import ar.edu.unju.escmi.tp5.collections.CollectionFactura;
+import ar.edu.unju.escmi.tp5.dominio.Factura;
+
+public class SubmenuCliente {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Autenticacion");
+        System.out.println("Ingrese nombre: ");String nombre=sc.nextLine(); 
+        System.out.println("Ingrese contraseña: "); int passwd = sc.nextInt();
+        
+        if(CollectionCliente.autenticacion(nombre,passwd)){ // falta que lo hagan
+            int op;
+            do{
+                System.out.println("Menu de Cliente");
+                System.out.println("1- Buscar factura");
+                System.out.println("2- Salir");
+                op = sc.nextInt(); sc.nextLine();
+            }while(op!=2);
+
+            switch(op){
+                case 1: {
+                    System.out.println("Ingrese numero de factura: "); int nroFactura= sc.nextInt(); sc.nextLine();
+                    Factura f = CollectionFactura.buscar(nroFactura);
+                    if( f !=null){
+                        Factura.mostrar(f); // falta q lo hagan
+                    } else {
+                        System.out.println("Factura no encontrada");
+                    }
+                    break;
+                }
+                case 2: System.out.println("Saliendo del rol cliente..."); break;
+                default: System.out.println("Opcion invalida"); break;
+            }
+        }
+        else{
+            System.out.println("Autenticacion fallida. Intente mas tarde");
+        }
+        sc.close();
+    }
+}
