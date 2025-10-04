3e052bb30fe1c04b451d8b17ff5b3535b80047e9
altarifa <abrillucerotarifa@gmail.com>
2025-10-04 20:01:00 -0300

    Collections y precarga de empleados Abril Tarifa

--- src/ar/edu/unju/escmi/tp5/collections/CollectionEmpleado.java
+++ src/ar/edu/unju/escmi/tp5/collections/CollectionEmpleado.java
@@ -1,32 +1,25 @@
 package ar.edu.unju.escmi.tp5.collections;
 
+
 import java.util.ArrayList;
 import ar.edu.unju.escmi.tp5.dominio.Empleado;
 
+
 public class CollectionEmpleado {
-    public static ArrayList<Empleado> empleados = new ArrayList<>();
+public static ArrayList<Empleado> empleados = new ArrayList<>();
 
-    public static void agregar(Empleado e) {
-        if (e != null && !empleados.contains(e)) {
-            empleados.add(e);
-        }
-    }
 
-    public static Empleado buscar(String usuarioEmpleado) {
-        for (Empleado e : empleados) {
-            if (e != null && e.getUsuarioEmpleado().equalsIgnoreCase(usuarioEmpleado)) {
-                return e;
-            }
-        }
-        return null;
-    }
+public static Empleado buscar(String usuarioEmpleado) {
+for (Empleado e : empleados) {
+if (e != null && e.getUsuarioEmpleado().equalsIgnoreCase(usuarioEmpleado)) {
+return e;
+}
+}
+return null;
+}
 
-    public static boolean eliminar(String usuarioEmpleado) {
-        Empleado e = buscar(usuarioEmpleado);
-        return e != null && empleados.remove(e);
-    }
 
-    public static ArrayList<Empleado> listar() {
-        return empleados;
-    }
+public static ArrayList<Empleado> listar() {
+return empleados;
+}
 }
